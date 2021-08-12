d5904c03c35d33260fedb6f937b817d169ae609e
SKKomolkina <skomolkina@gmail.com>
2021-08-12 16:11:06 +0300

    fix#98y98y

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
-// import { api } from '../utils/Api';
 
 import PopupWithForm from './PopupWithForm';
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { api } from './../utils/Api';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 import Card from './Card.js';
