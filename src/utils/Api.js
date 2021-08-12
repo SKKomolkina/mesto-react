33eed3b9487c096eabcfbf12d341b5f9db9d7b32
SKKomolkina <skomolkina@gmail.com>
2021-08-12 15:57:34 +0300

    fix for review-test

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -89,3 +89,11 @@ export default class Api {
         return Promise.reject(`Ошибка: ${res.status}`);
     }
 }
+
+export default api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-25',
+    headers: {
+        authorization: 'e0ccb70a-8300-4fff-b1a9-1044a866d1c7',
+        'Content-Type': 'application/json'
+    }
+});
