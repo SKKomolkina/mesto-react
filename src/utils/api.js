7ca33a1b6487ebc6e893417ef5e8a4cae8bf0a1b
SKKomolkina <skomolkina@gmail.com>
2021-08-12 16:13:01 +0300

    fix#99999

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export default class Api {
+class Api {
     constructor(options) {
         this._url = options.baseUrl;
         this._headers = options.headers;
@@ -90,12 +90,10 @@ export default class Api {
     }
 }
 
-const api = new Api({
+export default api = new Api({
     baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-25',
     headers: {
         authorization: 'e0ccb70a-8300-4fff-b1a9-1044a866d1c7',
         'Content-Type': 'application/json'
     }
 });
-
-export default { api };
